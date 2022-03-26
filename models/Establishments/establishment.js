bbe94747fba9c7136b3f79c410719a088a4c6ff7
anthonyjames <anthonysemaj23@gmail.com>
2022-03-26 13:12:18 +0800

    fixed establishment model name

--- models/Establishments/establishment.js
+++ models/Establishments/establishment.js
@@ -53,15 +53,13 @@ establishmentSchema.set('toJSON', {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
-        // the passwordHash should not be revealed
-        delete returnedObject.passwordHash
     }
 })
 
 establishmentSchema.plugin(uniqueValidator)
 
 
-const Establishment = establishmentConnection.model('EstablishmentUser', establishmentSchema)
+const Establishment = establishmentConnection.model('Establishment', establishmentSchema)
 
 
 module.exports =  Establishment
