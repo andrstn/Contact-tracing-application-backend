0e49f52a5b5d3ed5c8f4374d22a722b38829c50f
andrestine <andrestineboter15@gmail.com>
2022-03-28 14:06:59 +0800

    added endpoint: update username and password

--- controllers/Users/establishment-users.js
+++ controllers/Users/establishment-users.js
@@ -72,4 +72,45 @@ usersEstablishmentRouter.post('/log-in', async (request, response) => {
   })
 })
 
+// Update username
+usersEstablishmentRouter.put('/:id/change-username', async (request, response) => {
+  const { username } = request.body
+
+  const updateUser = {
+    username: username
+  }
+
+  await EstablishmentUser.findByIdAndUpdate(request.params.id, updateUser, { new: true })
+  response.status(201).json({
+    message: 'Username updated'
+  })
+})
+
+// Update password
+usersEstablishmentRouter.put('/:id/change-password', async (request, response) => {
+  const { username, oldPassword, newPassword } = request.body
+
+  const user = await EstablishmentUser.findOne({username: username})
+  const oldPasswordCorrect = user === null
+      ? false
+      : await bcrypt.compare(oldPassword, user.passwordHash)
+
+  if (!oldPasswordCorrect) {
+    return response.status(401).json({error: 'invalid old password'})
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(newPassword, saltRounds)
+
+  const updateUser = {
+    username: username,
+    passwordHash: passwordHash
+  }
+
+  await EstablishmentUser.findByIdAndUpdate(request.params.id, updateUser, { new: true })
+  response.status(201).json({
+    message: 'Password updated'
+  })
+})
+
 module.exports = usersEstablishmentRouter
