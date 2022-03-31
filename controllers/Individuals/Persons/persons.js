06890312bd9620f8ace04d53f8af2e63fd00d4ef
andrestine <andrestineboter15@gmail.com>
2022-04-01 00:07:57 +0800

    initiated adding transaction

--- controllers/Individuals/Persons/persons.js
+++ controllers/Individuals/Persons/persons.js
@@ -12,9 +12,32 @@ const getTokenFrom = request => {
     return null
   }
 
-  // Get all profiles
-  personsRouter.get('/', async (request, response) => {
-    try {
+// Get all profiles
+personsRouter.get('/', async (request, response) => {
+  try {
+    const persons = await Individual
+      .find({})
+      .populate('transactionLevelOne',{
+          
+      })
+      .populate('transactionLevelTwo',{
+
+      })
+      .populate('transactionLevelThree',{
+
+      })
+
+    response.json(persons)
+  } catch (error) {
+    return response.status(401).json({
+      error: 'Failed to retrieve profiles'
+    })
+  }
+})
+
+//Gets specific profile
+personsRouter.get('/:id', async (request, response) => {
+  try {
       const persons = await Individual
         .find({})
         .populate('Individual',{
@@ -27,63 +50,75 @@ const getTokenFrom = request => {
             contactNumber: 1,
             email: 1,
             status: 1,
+            province: 1,
+            city: 1,
+            barangay: 1,
+            street: 1,
+            resident:1 ,
+            special: 1,
+            transactionLevelOne: 1,
+            transactionLevelTwo: 1,
+            transactionLevelThree: 1
         })
       response.json(persons)
     } catch (error) {
       return response.status(401).json({
-        error: 'Failed to retrieve profiles'
+        error: 'Failed to retrieve profile'
       })
     }
+})
+
+// Profile Sign-up
+personsRouter.post('/sign-up', async (request, response) => {
+  const body = request.body
+  const token = getTokenFrom(request)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken) {
+    return response.status(401).json({
+      error: 'Token missing or invalid.'
+    })
+  }
+
+  const user = await IndividualUser.findById(decodedToken.id)
+
+  const person = new Individual({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    middleName: body.middleName,
+    suffix: body.suffix,
+    gender: body.gender,
+    birthDate: body.birthDate,
+    contactNumber: body.contactNumber,
+    email: body.email,
+    status: body.status,
+    province: body.province,
+    city: body.city,
+    barangay: body.barangay,
+    street: body.street,
+    resident: body.resident,
+    special: body.special,
+    transactionLevelOne: [],
+    transactionLevelTwo: [],
+    transactionLevelThree: [],
+    user: user._id
   })
 
-  //Gets specific profile
-  personsRouter.get('/:id', async (request, response) => {
-    try {
-        const persons = await Individual
-          .find({})
-          .populate('Individual',{
-              firstName: 1,
-              lastName: 1,
-              middleName: 1,
-              suffix: 1,
-              gender: 1,
-              birthDate: 1,
-              contactNumber: 1,
-              email: 1,
-              status: 1,
-              province: 1,
-              city: 1,
-              barangay: 1,
-              street: 1,
-              resident:1 ,
-              special: 1,
-              transactionLevelOne: 1,
-              transactionLevelTwo: 1,
-              transactionLevelThree: 1
-          })
-        response.json(persons)
-      } catch (error) {
-        return response.status(401).json({
-          error: 'Failed to retrieve profile'
-        })
-      }
-  })
-
-  // Profile Sign-up
-  personsRouter.post('/sign-up', async (request, response) => {
-    const body = request.body
-    const token = getTokenFrom(request)
-
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken) {
+  try {
+    const savedPerson = await person.save()
+      response.status(201).json(savedPerson)
+  } catch (error) {
       return response.status(401).json({
-        error: 'Token missing or invalid.'
-      })
-    }
+        error: 'Failed to complete profile.'
+    })
+  }
+})
 
-    const user = await IndividualUser.findById(decodedToken.id)
-  
-    const person = new Individual({
+// Update profile
+personsRouter.put('/:id/update-profile', async (request, response) => {
+  const body = request.body
+
+  const person = {
       firstName: body.firstName,
       lastName: body.lastName,
       middleName: body.middleName,
@@ -99,46 +134,10 @@ const getTokenFrom = request => {
       street: body.street,
       resident: body.resident,
       special: body.special,
-      transactionLevelOne: [],
-      transactionLevelTwo: [],
-      transactionLevelThree: [],
-      user: user._id
-    })
-  
-    try {
-      const savedPerson = await person.save()
-       response.status(201).json(savedPerson)
-    } catch (error) {
-        return response.status(401).json({
-         error: 'Failed to complete profile.'
-      })
-    }
-  })
+  }
 
-  // Update profile
-  personsRouter.put('/:id/update-profile', async (request, response) => {
-    const body = request.body
-  
-    const person = {
-        firstName: body.firstName,
-        lastName: body.lastName,
-        middleName: body.middleName,
-        suffix: body.suffix,
-        gender: body.gender,
-        birthDate: body.birthDate,
-        contactNumber: body.contactNumber,
-        email: body.email,
-        status: body.status,
-        province: body.province,
-        city: body.city,
-        barangay: body.barangay,
-        street: body.street,
-        resident: body.resident,
-        special: body.special,
-    }
-  
-    const updatedProfile = await Individual.findByIdAndUpdate(request.params.id, person, { new: true })
-    response.status(201).json(updatedProfile)
-  })
+  const updatedProfile = await Individual.findByIdAndUpdate(request.params.id, person, { new: true })
+  response.status(201).json(updatedProfile)
+})
 
   module.exports = personsRouter
